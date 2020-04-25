9546f66beee986f0972cdab34f06525606f8cf97
Mathew Sheets <mathew.sheets@gmail.com>
2020-04-24 21:26:22 -0400

    Dropped version

--- UIKitExtensions/UIKitExtensions/NSObject+Extensions.swift
+++ UIKitExtensions/UIKitExtensions/NSObject+Extensions.swift
@@ -11,10 +11,12 @@ import Foundation
 extension NSObject {
     
     var className: String {
+        
         return String(describing: type(of: self))
     }
     
     class var className: String {
+        
         return String(describing: self)
     }
 }
